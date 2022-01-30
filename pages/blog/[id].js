38c2c8244ec0878c62d83849751361a1edbe1a89
ryosuke sato <ryo-sato@wd-flat.com>
2022-01-30 18:02:43 +0900

    詳細ページカテゴリー追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -6,6 +6,7 @@ export default function BlogId({ blog }) {
     <main>
       <h1>{blog.title}</h1>
       <p>{blog.publishedAt}</p>
+      <p>{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
